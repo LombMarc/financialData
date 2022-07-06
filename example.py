452784f4163be7d8e2b6daaa456d5997161bec64
Marco Lombardi <63810688+LombMarc@users.noreply.github.com>
2022-07-06 11:07:44 +0200

    Update and rename main.py to example.py
    brief explanation

--- example.py
+++ example.py
@@ -8,9 +8,12 @@ ak = os.getenv('FMP_KEY')  #https://financialmodelingprep.com free api key
 nk = os.getenv('NEWS_KEY') #https://newsapi.org free api key plan
 opKey = os.getenv('EODKEY') #https://eodhistoricaldata.com premium api key plan (20$ for option data)
 
+# The api key can be saved as environmental variable or stored as a string variable in the script itself
+
 if __name__=='__main__':
-    '''tick_list = ticker_list(ak)
-    print(len(tick_list))'''
-    #apl = Stock('AAPL')
+    apl = Stock('AAPL')
+    print(apl)
     apl = Options('AAPL')
-    print(apl.loc['2022-05-13'])
+    print(apl.iloc[0])
+    
+# This example will retrive the overall summary for the Apple stocks and then will show information for the option with smaller time to maturity.
